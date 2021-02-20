4688f05d55fa35fb0b9719a7043572068263a006
kyamashita5 <pagbeta@outlook.jp>
2021-02-20 21:40:26 +0900

    長い直線の途中のノードを無視

--- generate_shortcut_path.py
+++ generate_shortcut_path.py
@@ -9,27 +9,24 @@ from smooth_path import smooth_path
 
 #pts = np.loadtxt('./data/robotena_points.txt', delimiter=' ')
 #pts = np.loadtxt('./data/2019kansai_points.txt', delimiter=' ')
-pts = np.loadtxt('./data/2015alljapan_points.txt', delimiter=' ')
-
+pts = np.loadtxt('./data/2017alljapan_points.txt', delimiter=' ')
 
 # initialize nodes
-nodes = [pts, smooth_path(pts)]
-#for dn in [-0.05, -0.025, 0.025, 0.05]:
-#    pts_offset = np.zeros_like(pts)
-#    for idx, pt in enumerate(pts):
-#        ang = course_angle(pts, idx)
-#        ofs = np.array([-np.sin(ang), np.cos(ang)])
-#        pts_offset[idx] = pts[idx] + dn * ofs
-#    nodes.append(pts_offset)
-nodes = np.stack(nodes, axis=1) # [N,K,2]
+nodes = [pts, smooth_path(pts)] # ライン上の点，およびラインを平滑化した経路上の点をノードとして利用
+nodes = np.stack(nodes, axis=1) # [N,2,2] (コース長)×(各地点におけるノードの個数)×(xy)
 
+# ラインおよび平滑化経路を描画
 for k in range(nodes.shape[1]):
     plt.plot(nodes[:,k,0], nodes[:,k,1])
 plt.axes().set_aspect('equal')
 plt.grid()
 plt.show()
 
-def get_adjacent(idx_x, idx_y, nodes, dl = 0.01, margin = 0.05, ang_th = np.pi/180.0, curv_lim = 10.0):
+idx_min = 0 # グローバル変数．これより手前のノードを終点とするアークは無視する
+
+# ラインまたは平滑化経路に接する直線・円弧をアークとして利用する
+def get_adjacent(idx_x, idx_y, nodes, dl = 0.01, margin = 0.05, ang_th = np.pi/180.0, curv_lim = 10.0, max_num_adjacent=200):
+    global idx_min
     ang = course_angle(nodes[:,idx_y], idx_x)
     pt0 = nodes[idx_x,idx_y]
     list_adj = []
@@ -63,7 +60,7 @@ def get_adjacent(idx_x, idx_y, nodes, dl = 0.01, margin = 0.05, ang_th = np.pi/1
             ang1_ = ang + (ang_radius if (c > 0.0) else -ang_radius)
             err_ang = np.abs(np.mod(ang1 - ang1_ + np.pi, 2.0  * np.pi) - np.pi)
 
-            if (err_ang < ang_th) and (cmin < c) and (cmax > c): # and (idx_i >= idx_min)
+            if (err_ang < ang_th) and (cmin < c) and (cmax > c) and (idx_i >= idx_min):
                 # straight
                 if (ang_radius < (np.pi / 180.0)):
                     l = np.sqrt(dif_t * dif_t + dif_n * dif_n)
@@ -73,11 +70,18 @@ def get_adjacent(idx_x, idx_y, nodes, dl = 0.01, margin = 0.05, ang_th = np.pi/1
                     l = np.sqrt(dif_t * dif_t + dif_n * dif_n) / np.sinc(0.5 * ang_radius / np.pi)
                     list_adj.append([cost_turn(l, c), [idx_i, idx_j], [idx_x, idx_y]])
             
-            # TODO: 枝刈り
-            
         idx_i += 1
+
+    # アークの数が多い(=長いアークがある)場合は距離の短いアークを無視
+    # 長い直線や大Rの途中を始点とするショートカットは存在しない(たぶん)
+    while len(list_adj) > max_num_adjacent:
+        if idx_min < list_adj[0][1][0]:
+            idx_min = list_adj[0][1][0]
+        list_adj.pop(0)
+    
     return list_adj
 
+# 2つのノードを結ぶアークを描画する
 def draw_arch(node0, node1, nodes, dl = 0.001, ang_th = np.pi/180.0):
     ang0 = course_angle(nodes[:,node0[1]], node0[0])
     pt0 = nodes[node0[0], node0[1]]
@@ -113,6 +117,7 @@ def draw_arch(node0, node1, nodes, dl = 0.001, ang_th = np.pi/180.0):
 
 dl = 0.01
 
+# スタート地点から伸びるアークを描画
 idx_x0 = 0
 idx_y0 = 0
 list_adj = get_adjacent(idx_x0, idx_y0, nodes, dl)
@@ -124,8 +129,8 @@ plt.axes().set_aspect('equal')
 plt.grid()
 plt.show()
 
-
-print('start computing shortest path')
+# ダイクストラ法でショートカット経路を導出する
+print('start searching shortcut path')
 start = time.time()
 costs = -1*np.ones_like(nodes[:,:,0])
 decided = np.zeros_like(nodes[:,:,0])
